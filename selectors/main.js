3031625ccf1dfda0bf5bca40d9fb546450cff4e9
picorana <dibartolomeo.sara@gmail.com>
2020-01-16 11:42:04 -0500

    first commit

---
+++ selectors/main.js
@@ -0,0 +1,24 @@
+//d3.select(".house").text("Targaryen")
+//d3.select(".name").text("Aegon")
+//d3.select("tr:nth-child(4) .char").text("Evil")
+//d3.selectAll(".rate").text("3")
+
+
+//d3.select("tr:nth-child(4) .char")
+//	.html("<strong>Evil<strong>")
+
+//Let's add a span to this
+
+// d3.select("tr:nth-child(4) .char")
+//   .html("<strong>Evil<strong>")
+//   .append('span')
+//   .html(" Surprise!");
+
+//beginning of the element
+//d3.select("tr:nth-child(4) .char")
+//	.html("<strong>Evil<strong>")
+//  .insert ('span',':first-child')
+//  .html("SO ");
+
+//d3.selectAll("tr:nth-child(5)")
+//	.remove()
